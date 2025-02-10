7b8bc1bb19523951b100e07eb693c2db12b19145
NoahBilmer <noahbilmer@gmail.com>
2025-02-10 11:35:30 -0500

    Added comments

--- src/Game.cpp
+++ src/Game.cpp
@@ -20,7 +20,6 @@ Game::Game() {
     SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR);  // Texture scale filter to use
 
     map.loadMap("map");
-
     SetTargetFPS(60);
 }
 
@@ -42,49 +41,45 @@ void Game::doLogic() {
 void Game::render() {
     float scale = MIN((float)GetScreenWidth() / screenWidth, (float)GetScreenHeight() / screenHeight);
     Vector2 position = player.getPosition();
-    BeginTextureMode(target);
-
-    ClearBackground(RAYWHITE);
-
-    char positionStr[50];
-    std::snprintf(positionStr, 50, "Position: (%f,%f)", position.x, position.y);
-    char lookStr[50];
-    std::snprintf(lookStr, 50, "Rotation: (%f)",player.getRotation());
     Color lightBlue = Color{ 64, 96, 145,255 };
     Color darkBlue = Color{ 12, 24, 41,255 };
+    BeginTextureMode(target);
+        ClearBackground(RAYWHITE);
+        char positionStr[50];
+        std::snprintf(positionStr, 50, "Position: (%f,%f)", position.x, position.y);
+        char lookStr[50];
+        std::snprintf(lookStr, 50, "Rotation: (%f)",player.getRotation());
+        // Create the background 
+        DrawRectangleGradientV(0, 0, screenWidth, screenHeight / 2, lightBlue, darkBlue);
+        DrawRectangleGradientV(0, screenHeight / 2, screenWidth, screenHeight / 2, darkBlue, lightBlue);
+        // Draw the view.
+        drawView();
 
-    // Create the background 
-    DrawRectangleGradientV(0, 0, screenWidth, screenHeight / 2, lightBlue, darkBlue);
-    DrawRectangleGradientV(0, screenHeight / 2, screenWidth, screenHeight / 2, darkBlue, lightBlue);
-    // Draw the view.
-    drawView();
-
-    DrawText(positionStr, 10, 50, 30, LIGHTGRAY);
-    DrawText(lookStr, 10, 75, 30, LIGHTGRAY);
-    DrawFPS(10, 10);
+        DrawText(positionStr, 10, 50, 30, LIGHTGRAY);
+        DrawText(lookStr, 10, 75, 30, LIGHTGRAY);
+        DrawFPS(10, 10);
    
-    if (0) {
+        if (0) {
         
-        for (Line line : map.getMap()) {
-            DrawLineEx(line.p1, line.p2, 3, line.color);
+            for (Line line : map.getMap()) {
+                DrawLineEx(line.p1, line.p2, 3, line.color);
+            }
+        
+            rayCaster.castRays();
+            std::unordered_set<Ray2d> rays = rayCaster.getRays();
+            DrawLineEx(rayCaster.getPosition(), Vector2Scale(getRayFromAngle(rayCaster.getRotation() * DEG2RAD), rayCaster.getRayLength()), 2, YELLOW);
+            for (auto const ray : rays)
+                DrawLineEx(rayCaster.getPosition(), ray.point, 1, RED);
+
         }
-        
-        rayCaster.castRays();
-        std::unordered_set<Ray2d> rays = rayCaster.getRays();
-        DrawLineEx(rayCaster.getPosition(), Vector2Scale(getRayFromAngle(rayCaster.getRotation() * DEG2RAD), rayCaster.getRayLength()), 2, YELLOW);
-        for (const auto ray : rays)
-            DrawLineEx(rayCaster.getPosition(), ray.point, 1, RED);
-
-    }
-  
     EndTextureMode();
-    BeginDrawing();
-    DrawTexturePro(target.texture, Rectangle{ 0.0f, 0.0f, (float)target.texture.width, (float)-target.texture.height },
-             Rectangle{ (GetScreenWidth() - ((float)screenWidth * scale))*0.5f, (GetScreenHeight() - ((float)screenHeight * scale))*0.5f,
-             (float)screenWidth * scale, (float)screenHeight * scale },
-             Vector2{ 0, 0 }, 0.0f, WHITE);
-   
-    ClearBackground(BLACK);
+        BeginDrawing();
+        DrawTexturePro(target.texture, Rectangle{ 0.0f, 0.0f, (float)target.texture.width, (float)-target.texture.height },
+                 Rectangle{ (GetScreenWidth() - ((float)screenWidth * scale))*0.5f, (GetScreenHeight() - ((float)screenHeight * scale))*0.5f,
+                 (float)screenWidth * scale, (float)screenHeight * scale },
+                 Vector2{ 0, 0 }, 0.0f, Color{255,255,255,255});
+ 
+        ClearBackground(BLACK);
     EndDrawing();
 }
 
@@ -113,42 +108,43 @@ void Game::drawView() {
     float dist;
     int count = 0;
     int wallSize = 20;
+    int wallHeight = 96;
     float intensity;
-    Vector2 vec1 = { 0, 0,};
-    Vector2 vec2 = { 0,screenHeight / 2 - 150 };
+    Vector2 lineStart = { 0, 0,};
+    Vector2 lineEnd = { 0,screenHeight / 2 - 150 };
     Vector2 rayVector = { 0,0 };
     Color colorVal;
 
-   for (const auto ray : rays) {
+   for (auto const ray : rays) {
        
         rayVector.x = abs(ray.point.x - player.getPosition().x);
         rayVector.y = abs(ray.point.y - player.getPosition().y);
-
-        dist = sqrt(rayVector.x * rayVector.x + rayVector.y * rayVector.y) + 1;
-        float lineSize = ((96 / dist) * 277) + wallSize;
-       
-        vec1.y = ((screenHeight / 2) + (lineSize / 2));
-        vec2.y = vec1.y - lineSize;
-
+        // Get the distance (Add a tiny constant so we never divide by 0)
+        dist = sqrt(rayVector.x * rayVector.x + rayVector.y * rayVector.y);
+        float lineSize = ((wallHeight / dist) * 277) + wallSize;
+        // for the y positions, we start the line in the middle of the screen
+        // plus half the line height so the line is centered. 
+        lineStart.y = ((screenHeight / 2) + (lineSize / 2));
+        lineEnd.y = lineStart.y - lineSize;
+        
         colorVal.r = ray.color.r;
         colorVal.g = ray.color.g;
         colorVal.b = ray.color.b;
         if (IsKeyDown(KEY_F)) {
             printf("debug\n");
         }
-        vec1.x = ray.index;
-        vec2.x = ray.index;
-       
-        vec1.x = ray.index * lineWidth;
-        vec2.x = ray.index * lineWidth;
         
+        lineStart.x = ray.index * lineWidth;
+        lineEnd.x = ray.index * lineWidth;
+        
+        // To determine the intensity of the color we divide some constant by the distance,
+        // and then normalize that value between 0 and 1.
         intensity = 170 / (dist);
         intensity = Normalize(intensity, 0, 1);
-       
         colorVal.b = Clamp(colorVal.b * intensity, 0 ,ray.color.b);
         colorVal.g = Clamp(colorVal.g * intensity, 0, ray.color.g);
         colorVal.r = Clamp(colorVal.r * intensity, 0, ray.color.r); 
-        DrawLineEx(vec1, vec2, lineWidth , colorVal);
+        DrawLineEx(lineStart, lineEnd, lineWidth , colorVal);
         
     }
 }
